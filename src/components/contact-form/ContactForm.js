1afad1925f086079fba4495eeb097f772670fadb
Pavlo-V-M <letter.screen.1@gmail.com>
2023-06-20 23:39:36 +0300

    try-2

--- src/components/contact-form/ContactForm.js
+++ src/components/contact-form/ContactForm.js
@@ -1,61 +1,5 @@
 // ContactForm.js
 
-// import React from 'react';
-// import { useDispatch } from 'react-redux';
-// import { nanoid } from 'nanoid';
-// import { addContact } from '../store/contactsSlice';
-
-// const ContactForm = () => {
-//   const dispatch = useDispatch();
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const form = event.target;
-//     const name = form.name.value;
-//     const number = form.contactsNumber.value;
-
-//     const newContact = {
-//       id: nanoid(),
-//       name,
-//       number,
-//     };
-
-//     dispatch(addContact(newContact));
-
-//     form.reset();
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <label htmlFor={nameInputID}>Name</label>
-//       <input
-//         type="text"
-//         name="name"
-//         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//         required
-//         value={name}
-//         onChange={handleChange}
-//         id={nameInputID}
-//       />
-//       <label htmlFor={numberInputID}>Number</label>
-//       <input
-//         type="tel"
-//         name="contactsNumber"
-//         pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
-//         title="Phone number format: 123-45-67"
-//         required
-//         value={contactsNumber}
-//         onChange={handleChange}
-//         id={numberInputID}
-//       />
-//       <button type="submit">Add contact</button>
-//     </form>
-//   );
-// };
-
-// export default ContactForm;
-
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { nanoid } from 'nanoid';
@@ -78,7 +22,10 @@ const ContactForm = () => {
 
     dispatch(addContact(newContact));
 
-    form.reset();
+    form.reset('');
+    // Clear the form state
+    setName('');
+    setContactsNumber('');
   };
 
   const handleNameChange = (event) => {
--- src/components/contact-list/ContactList.js
+++ src/components/contact-list/ContactList.js
@@ -1,38 +1,5 @@
 // ContactList.js
 
-// import PropTypes from 'prop-types';
-// import React from 'react';
-// import { useDispatch } from 'react-redux';
-// import { deleteContact } from '../store/contactsSlice';
-
-// const ContactList = (props) => {
-//   const { contacts } = props;
-//   const dispatch = useDispatch();
-
-//   const handleDeleteClick = (name) => {
-//     dispatch(deleteContact(name));
-//   };
-
-//   return (
-//     <div>
-//       <ul>
-//       {contacts.map(contact => (
-//         <li key={contact.name}>
-//           {contact.name}: <span>{contact.number}</span>
-//           <button type="button" onClick={() => onDelete(contact.name)}>Delete</button>
-//         </li>
-//       ))}
-//     </ul>
-//     </div>
-//   );
-// };
-
-// ContactList.propTypes = {
-//   contacts: PropTypes.array.isRequired,
-// };
-
-// export default ContactList;
-
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useDispatch } from 'react-redux';
@@ -50,7 +17,7 @@ const ContactList = (props) => {
     <div>
       <ul>
         {contacts.map((contact) => (
-          <li key={contact.name}>
+          <li key={contact.id}>
             {contact.name}: <span>{contact.number}</span>
             <button type="button" onClick={() => handleDeleteClick(contact.name)}>Delete</button>
           </li>
@@ -65,4 +32,15 @@ ContactList.propTypes = {
 };
 
 export default ContactList;
+
+// <div>
+    //   <ul>
+    //     {contacts.map((contact) => (
+    //       <li key={contact.name}>
+    //         {contact.name}: <span>{contact.number}</span>
+    //         <button type="button" onClick={() => handleDeleteClick(contact.name)}>Delete</button>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
 
